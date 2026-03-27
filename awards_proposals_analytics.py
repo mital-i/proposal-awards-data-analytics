991f0757156a023587a7b7dd212ab6905769c193
Mitali Mittal <mitaliactive@gmail.com>
2026-03-26 17:10:23 -0700

    changes

--- awards_proposals_analytics.py
+++ awards_proposals_analytics.py
@@ -434,10 +434,3 @@ with tab_tables:
             )
         else:
             st.caption("No awards uploaded.")
-
-
-def to_excel(df):
-    output = BytesIO()
-    with pd.ExcelWriter(output, engine="xlsxwriter") as writer:
-        df.to_excel(writer, index=False, sheet_name="Sheet1")
-    return output.getvalue()
